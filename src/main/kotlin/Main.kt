b78eb84814032ce192b981e12672bb53424f0ecf
Dgriscow <dgrischow@students.mchenry.edu>
2023-08-16 15:13:48 -0500

    Actually add this to github

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,3 +2,4 @@
 "this is a github push test"
         "there is no working code here, as i cant get it to print, Cant wait to learn how we do that"
         "on another note, This software looks great and fun to use."
+        ""
